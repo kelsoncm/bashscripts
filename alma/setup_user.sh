b4bd3921eb94680082d3576e8848515b23f76e9f
kelsoncm <kelsoncm@gmail.com>
2025-05-07 14:17:26 -0300

    fix: install oh-my-zsh para o usuário

--- alma/setup_user.sh
+++ alma/setup_user.sh
@@ -5,7 +5,7 @@
 chsh -s /bin/zsh
 chsh -s /bin/zsh
 zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+CHSH=no RUNZSH=no zsh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
